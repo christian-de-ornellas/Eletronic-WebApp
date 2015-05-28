722658efb94e7c3f70e0f2073aa898db3435914b
Christian de Ornellas Possidonio <ch_ris@hotmail.com>
2015-05-27 23:19:50 -0300

    Insert icon
    Now go Insert icon

--- index.php
+++ index.php
@@ -51,13 +51,13 @@
       <ul class="nav navbar-nav navbar-right">
         <li><a href="#">Link</a></li>
         <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="glyphicon glyphicon-user"></i><span class="caret"></span></a>
           <ul class="dropdown-menu" role="menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
+            <li><a href="#">Perfil</a></li>
+            <li><a href="#">Alterar</a></li>
             <li class="divider"></li>
-            <li><a href="#">Separated link</a></li>
+             <li><a href="#">Sair do Sistema</a></li>
+           
           </ul>
         </li>
       </ul>
