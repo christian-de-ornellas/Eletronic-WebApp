fb9e21ec0393e1ecc575f9f4fc73f2136b41429a
Christian de Ornellas Possidonio <ch_ris@hotmail.com>
2015-05-28 00:13:52 -0300

    System Hours with date
    System Hours with date

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 <body>
 <div class="container">
 <header><div class="header-logotipo"><img src="img/logotipo.png" width="200"></div></header>
-<nav class="navbar navbar-default">
+<nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
@@ -23,15 +23,13 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#">Menu</a>
+      <a class="navbar-brand" href="#"><i class="fa fa-home"></i></a>
     </div>
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li class="active"><a href="#"><i class="glyphicon glyphicon-plus"></i>Cadastrar<span class="sr-only">(current)</span></a></li>
-        <li><a href="#">Link</a></li>
         <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"> <i class=""></i><i class="fa fa-user-plus"></i></i> Cadastrar <span class="caret"></span></a>
           <ul class="dropdown-menu" role="menu">
             <li><a href="#">Action</a></li>
             <li><a href="#">Another action</a></li>
@@ -42,22 +40,46 @@
             <li><a href="#">One more separated link</a></li>
           </ul>
         </li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-folder-open"></i> Consultar<span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+            <li class="divider"></li>
+            <li><a href="#">One more separated link</a></li>
+          </ul>
+        </li>
+         <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"> <i class="fa fa-bar-chart"></i> Relatório <span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+            <li class="divider"></li>
+            <li><a href="#">One more separated link</a></li>
+          </ul>
+        </li>
+        <li><a href="#"><i class="fa fa-envelope-o"></i> Enviar e-mail ao suporte</a></li>
+        
       </ul>
-      <form class="navbar-form navbar-left" role="search">
-        <div class="form-group">
-          <input type="text" class="form-control" placeholder="Search">
-        </div>
-        <button type="submit" class="btn btn-default">Submit</button>
-      </form>
+    
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="#">Link<i class="fa fa-calculator"></i></a></li>
+        <li><a href="#"> <i class="fa fa-clock-o"></i> <?php
+$dataHora = date("d/m/Y h:i:s");
+echo $dataHora;
+?></a></li>
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="glyphicon glyphicon-user"></i><span class="caret"></span></a>
           <ul class="dropdown-menu" role="menu">
-            <li><a href="#">Perfil</a></li>
-            <li><a href="#">Alterar</a></li>
+            <li><a href="#"><i class="fa fa-share-square-o"></i> Perfil</a></li>
+            <li><a href="#"><i class="fa fa-pencil-square-o"></i> Alterar</a></li>
             <li class="divider"></li>
-             <li><a href="#">Sair do Sistema</a></li>
+             <li><a href="#"><i class="fa fa-power-off"></i> Sair do Sistema</a></li>
            
           </ul>
         </li>
