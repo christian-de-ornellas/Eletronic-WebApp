caecbc43397eaa8f21f908f817bcb6c9482b9586
Christian de Ornellas Possidonio <ch_ris@hotmail.com>
2015-06-01 13:45:11 -0300

    Cupom
    The cupom

--- imprimir-cupon.php
+++ imprimir-cupon.php
@@ -2,86 +2,43 @@
 <!--HTML5 & PHP - ELETRONIC -
 ANALISTA DESENVOLVEDOR  -CHRISTIAN DE ORNELLAS POSSIDONIO--->
 <link rel="stylesheet" type="text/css" href="css/custom.css">
-
-
+<link rel="alternate" type="text/css" midia="print" href="css/imprimir.css">
 <?php
-
 function __autoload($class_name) {
     require_once 'Controller/' . $class_name . '.php';
 }
 ?>
-
-
-
-<div id="ImprimirConteudo">
-    <pre>
-	<table class="table-cupon"> 
-          
-            <tr>
-                <td><img src="img/logotipo.png" width="200"></td>  
-                
-            </tr>
-            
-            <tr>
-            <td>CPNJ: 00.264.478/0001-29 <br />
-                Av. 22 de maio, 6224<br />
-                Nova Cidade - Itaboraí - RJ
-                </td>  
-            </tr>
-            
-            <tr>
-                <td class="td-cupon"> Promoção : Dia dos namorados
-                </td>  
-            </tr>
-            
-            
-            <tr>
-                <td class="td-cupon"> Premio : Um veículo FIAT PALIO FIRE 1.0 - com a cor Branco 02 portas 2014/2015 0km.
-                </td>  
-            </tr>
-            
-            <tr>
-                <td class=""> Participe da promoção válida até 01/04/2015.
-                </td>  
-            </tr>
-            
-            <tr>
-                <td class=""> O Sorteio será realizado no dia 02/04/2015 às 16h.
-                </td>  
-            </tr>
-            
-            <tr>
-                <td class=""> Nome:Christian de Ornellas Possidonio
-                </td>  
-            </tr>
-            
-            <tr>
-                <td class=""> E-mail:ch_ris@hotmail.com 
-                </td>  
-            </tr>
-            
-             <tr>
-                <td class="">Cpf: 141.250.097-40
-                </td>  
-            </tr>
-            
-             
-             <tr>
-                <td class="">Rg: 23.571.463-1
-                </td>  
-            </tr>
-            
-              <tr>
-                <td class="td-cupon">Codigo: 31052015001
-                </td>  
-            </tr>
-            
-            
-            
-</table>
+<center>
+<table class="table-cupon"> 
+    <tr>
+        <td><img src="img/logotipo.png" width="150"></td>                 
+    </tr
+    <tr>
+        <td class="bordas">
+        CPNJ: 00.264.478/0001-29 
+        Av. 22 de maio, 6224
+        Nova Cidade - Itaboraí - RJ
+        Nome:Christian de Ornellas Possidonio
+            E-mail:ch_ris@hotmail.com 
+            Rg: 23.571.463-1
+    </td>
+    </tr>           
+    <tr>
+        <td class="bordas">
+        Premio : Um veículo FIAT PALIO FIRE 1.0 - com a cor Branco 02 portas 2014/2015 0km.
+        Participe da promoção válida até 01/04/2015.  
+        O Sorteio será realizado no dia 02/04/2015 às 16h.
+        Número do cupom: 31052015001
+        </td>
+    </tr>    
+</table><br />
+<div id="invisivel">
+    <input type="button" name="imprimir" value="Imprimir" onclick="window.print();">
 </div>
+</center>
 
-<input type="button" name="imprimir" value="Imprimir" onclick="window.print();">
+
+
 
 <!--<table class="table table-hover">
     
@@ -124,4 +81,3 @@ function __autoload($class_name) {
 
 
 
-       
